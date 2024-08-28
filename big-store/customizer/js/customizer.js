44626e79de80b4b9660b67426ac6d1a69e8aee86
iheeresh <heereshd@gmail.com>
2024-08-28 13:37:22 +0530

    theme limitation

--- big-store/customizer/js/customizer.js
+++ big-store/customizer/js/customizer.js
@@ -1,9 +1,9 @@
 
 jQuery(document).ready(function(){
 jQuery('#customize-control-big_store_above_header_layout,#customize-control-big_store_above_footer_layout').addClass("lite");	
-jQuery('#_customize-input-big_store_above_header_col1_set option[value="none"],#_customize-input-big_store_above_header_col1_set option[value="menu"],#_customize-input-big_store_above_header_col1_set option[value="widget"],#_customize-input-big_store_above_header_col1_set option[value="social"],#_customize-input-big_store_above_header_col2_set option[value="none"],#_customize-input-big_store_above_header_col2_set option[value="text"],#_customize-input-big_store_above_header_col2_set option[value="menu"],#_customize-input-big_store_above_header_col2_set option[value="widget"]').attr("disabled",true);
+jQuery('#_customize-input-big_store_above_header_col1_set option[value="none"],#_customize-input-big_store_above_header_col1_set option[value="menu"],#_customize-input-big_store_above_header_col1_set option[value="widget"],#_customize-input-big_store_above_header_col2_set option[value="none"],#_customize-input-big_store_above_header_col2_set option[value="social"],#_customize-input-big_store_above_header_col2_set option[value="menu"],#_customize-input-big_store_above_header_col2_set option[value="widget"]').attr("disabled",true);
 jQuery('#customize-control-big_store_above_mobile_disable input,#customize-control-big_store_move_to_top input').attr("disabled",true);
-jQuery('input[id=big_store_above_header_layout-abv-none],input[id=big_store_above_header_layout-abv-one],input[id=big_store_above_header_layout-abv-three],input[id=big_store_menu_alignmentcenter],input[id=big_store_menu_alignmentright],#customize-control-big_store_shadow_header input,#customize-control-big_store_sticky_header input,#customize-control-big_store_whislist_mobile_disable input,#customize-control-big_store_account_mobile_disable input,#customize-control-big_store_cart_mobile_disable input,input[id=big_store_above_footer_layout-ft-abv-none],input[id=big_store_above_footer_layout-ft-abv-one],input[id=big_store_above_footer_layout-ft-abv-two],input[id=big_store_above_footer_layout-ft-abv-three],#customize-control-big_store_main_header_option option[value="button"],#customize-control-big_store_main_header_option option[value="widget"],#customize-control-big_store_main_header_option option[value="callto"]').attr("disabled",true);
+jQuery('input[id=big_store_above_header_layout-abv-none],input[id=big_store_above_header_layout-abv-one],input[id=big_store_above_header_layout-abv-three],input[id=big_store_menu_alignmentcenter],input[id=big_store_menu_alignmentright],#customize-control-big_store_shadow_header input,#customize-control-big_store_sticky_header input,#customize-control-big_store_whislist_mobile_disable input,#customize-control-big_store_account_mobile_disable input,#customize-control-big_store_cart_mobile_disable input,input[id=big_store_above_footer_layout-ft-abv-none],input[id=big_store_above_footer_layout-ft-abv-one],input[id=big_store_above_footer_layout-ft-abv-three],#customize-control-big_store_main_header_option option[value="button"],#customize-control-big_store_main_header_option option[value="widget"],#customize-control-big_store_main_header_option option[value="callto"]').attr("disabled",true);
 
 // FrontPage
 // TopSlider
@@ -21,7 +21,8 @@ jQuery('#customize-control-big_store_single_row_slide_cat input,#customize-contr
 
 //Woo Category
 jQuery('#customize-control-big_store_cat_item_no input,#customize-control-big_store_category_slider_optn input').attr("disabled",true);
-
+jQuery('#customize-control-big_store_category_slide_list li input').attr("disabled",true);
+jQuery('#customize-control-big_store_category_slide_list li:nth-of-type(1) input,#customize-control-big_store_category_slide_list li:nth-of-type(2) input,#customize-control-big_store_category_slide_list li:nth-of-type(3) input,#customize-control-big_store_category_slide_list li:nth-of-type(4) input').attr("disabled",false);
 // ProducT Tab Image Carousel
 jQuery('#customize-control-big_store_product_img_sec_cat_list input').attr("disabled",true);
 jQuery('#customize-control-big_store_product_img_sec_cat_list li:nth-of-type(1) input,#customize-control-big_store_product_img_sec_cat_list li:nth-of-type(2) input,#customize-control-big_store_product_img_sec_cat_list li:nth-of-type(3) input').attr("disabled",false);
@@ -33,8 +34,8 @@ jQuery('#big_store_ribbon_backgroundvideo').attr("disabled",true);
 
 //Product Carousel 
 jQuery('#_customize-input-big_store_product_slide_optn option[value="featured"],#_customize-input-big_store_product_slide_optn option[value="random"]').attr("disabled",true);
-jQuery('#customize-control-big_store_single_row_prdct_slide input,#customize-control-big_store_product_slider_optn input').attr("disabled",true);
-
+jQuery('#customize-control-big_store_single_row_prdct_slide input,#customize-control-big_store_product_slider_optn input,#_customize-input-big_store_product_slider_cat option,#_customize-input-big_store_product_list_cat option').attr("disabled",true);
+jQuery('#_customize-input-big_store_product_slider_cat option:nth-of-type(1),#_customize-input-big_store_product_list_cat option:nth-of-type(1)').attr("disabled",false);
 //Banner
 jQuery('#big_store_banner_layout-bnr-two').attr("disabled",true);
 // Product List Carousel
@@ -44,5 +45,17 @@ jQuery('#customize-control-big_store_single_row_prdct_list input,#customize-cont
 jQuery('#customize-control-big_store_category_tab_list,#customize-control-big_store_product_img_sec_cat_list,#customize-control-big_store_include_category_slider').append("<h4>(To Select More Feature Available In Pro Version)</h4>");
 
 jQuery('#customize-control-big_store_top_slide_content,#customize-control-big_store_highlight_content').append("<h4>(To Add More Slides Feature Available In Pro Version)</h4>");
+
+jQuery('#customize-control-big_store_category_slide_list,#customize-control-big_store_product_slider_cat').append("<h4>(To Select More Feature Available In Pro Version)</h4>");
+
+
+// Big Store Theme limitaions
+jQuery('#customize-control-big_store_above_header_layout,#customize-control-big_store_above_footer_layout').addClass("lite");       
+    jQuery('input[id=big_store_above_header_layout-abv-none],input[id=big_store_above_header_layout-abv-one],input[id=big_store_above_header_layout-abv-three],input[id=big_store_top_slide_layout-slide-layout-5]').attr("disabled",true);
+
+    //Disable select option
+    jQuery('#_customize-input-big_store_blog_post_content option[value="full"],#_customize-input-big_store_blog_post_content option[value="nocontent"]').attr("disabled", true);
+    //Disable select input, toggle
+    jQuery('#customize-control-big_store_blog_read_more_txt input,#customize-control-big_store_rtl input,#customize-control-big_store_exclde_category input').attr("disabled", true);
 
 });

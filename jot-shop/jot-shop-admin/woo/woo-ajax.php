a7c9bbcc1fde41edd4fe07b8afc668f7e350a884
shivam kushwah <37370154+devkushwahshivam@users.noreply.github.com>
2024-12-07 18:46:15 +0530

    security issue fix

--- jot-shop/jot-shop-admin/woo/woo-ajax.php
+++ jot-shop/jot-shop-admin/woo/woo-ajax.php
@@ -7,6 +7,12 @@ if(!function_exists('jot_shop_cat_filter_ajax')){
 add_action('wp_ajax_jot_shop_cat_filter_ajax', 'jot_shop_cat_filter_ajax');
 add_action('wp_ajax_nopriv_jot_shop_cat_filter_ajax', 'jot_shop_cat_filter_ajax');
 function jot_shop_cat_filter_ajax(){
+if ( ! current_user_can( 'administrator' ) ) {
+  
+    wp_die( - 1, 403 );
+    
+} 
+check_ajax_referer('jotshop_nonce','nonce');  
 if(isset($_POST['data_cat_slug'])){
 $prdct_optn = get_theme_mod('jot_shop_category_optn','recent');
 $args = jot_shop_product_query(sanitize_key($_POST['data_cat_slug']),$prdct_optn);
--- m-shop/m-shop-admin/woo/woo-ajax.php
+++ m-shop/m-shop-admin/woo/woo-ajax.php
@@ -12,7 +12,14 @@ if( !function_exists('m_shop_cat_filter_ajax')){
 
 add_action('wp_ajax_m_shop_cat_filter_ajax', 'm_shop_cat_filter_ajax');
 add_action('wp_ajax_nopriv_m_shop_cat_filter_ajax', 'm_shop_cat_filter_ajax');
+
 function m_shop_cat_filter_ajax(){
+  if ( ! current_user_can( 'administrator' ) ) {
+  
+    wp_die( - 1, 403 );
+    
+} 
+check_ajax_referer('mshop_nonce','nonce'); 
 if(isset($_POST['data_cat_slug'])){
 $prdct_optn = get_theme_mod('m_shop_category_optn','recent');
 $args = m_shop_product_query(sanitize_key($_POST['data_cat_slug']),$prdct_optn);

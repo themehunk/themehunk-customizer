93421e623f963613ee290a4adc274138ec70abb1
iheeresh <heereshd@gmail.com>
2025-01-16 16:28:56 +0530

    UPDATED CODE

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '29013ffd834a1ac3055b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '315a4a328b0ddb367c55');

9c881ff69362b6afa31d4b61e2bcd14c209f2d88
iheeresh <heereshd@gmail.com>
2025-01-20 13:12:32 +0530

    code optimised

--- themehunk-customizer.php
+++ themehunk-customizer.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: ThemeHunk Customizer
   Description: With the help of ThemeHunk unlimited addon you can add unlimited number of columns for services, Testimonial, and Team with color options for each.
-  Version: 2.8.3
+  Version: 2.8.0
   Author: ThemeHunk
   Text Domain: themehunk-customizer
   Author URI: http://www.themehunk.com/
@@ -10,7 +10,7 @@
   if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
   
 // Version constant for easy CSS refreshes
-define('THEMEHUNK_CUSTOMIZER_VERSION', '2.8.3');
+define('THEMEHUNK_CUSTOMIZER_VERSION', '2.8.0');
 define('THEMEHUNK_CUSTOMIZER_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('THEMEHUNK_CUSTOMIZER_PLUGIN_PATH', plugin_dir_path(__FILE__) );
 include_once(plugin_dir_path(__FILE__) . 'notify/notify.php' );
@@ -23,38 +23,7 @@ function themehunk_customizer_text_domain(){
 }
 
 $theme = themehunk_customizer_text_domain(); 
-if(in_array("oneline-lite", $theme)){
-//require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
 
-}elseif(in_array("shopline", $theme)){
-// require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
-}elseif(in_array("featuredlite", $theme)){
-// require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
-}elseif(in_array("big-store", $theme)){
-require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
-}elseif(in_array("m-shop", $theme)){
-require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
-}
-elseif(in_array("jot-shop", $theme)){
-register_activation_hook( __FILE__, 'jot_shop_pro_deactivate' );
-require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
-}
-elseif(in_array("amaz-store", $theme)){
-register_activation_hook( __FILE__, 'themehunk_pro_plugin_deactivate' );
-require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
-}
-
-function jot_shop_pro_deactivate() {
-       require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-       deactivate_plugins( plugin_basename('jot-shop-pro/jot-shop-pro.php' ) );
-       
-    }
-function themehunk_pro_plugin_deactivate(){
-		 $theme = themehunk_customizer_text_domain(); 
-		 $theme_name = $theme[0];
-	require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-	deactivate_plugins( plugin_basename($theme_name.'-pro/'.$theme_name.'-pro.php' ) );
-}
 function themehunk_customizer_load_file(){
 	include_once(plugin_dir_path(__FILE__) . 'themehunk/customizer-font-selector/class/class-oneline-font-selector.php' );
     include_once(plugin_dir_path(__FILE__) . 'themehunk/customizer-range-value/class/class-oneline-customizer-range-value-control.php' );
@@ -71,36 +40,43 @@ function themehunk_customizer_load_plugin() {
 	include_once( plugin_dir_path(__FILE__) . 'themehunk/color-picker/color-picker.php' );
 	$theme = themehunk_customizer_text_domain(); 
 	if(in_array("oneline-lite", $theme)){
+		// require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
 		add_action('widgets_init', 'themehunk_customizer_widgets_init');
 		include_once( plugin_dir_path(__FILE__) . 'oneline-lite/include.php' );
 		
 	}elseif(in_array("featuredlite", $theme)){
+		// require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
 		add_action('widgets_init', 'themehunk_customizer_widgets_init');
 		include_once( plugin_dir_path(__FILE__) . 'featuredlite/include.php' );
 	}elseif(in_array("shopline", $theme)){
-
+		// require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
 		include_once( plugin_dir_path(__FILE__) . 'shopline/include.php' );
 		include_once(plugin_dir_path(__FILE__) . 'themehunk/customizer-tabs/class/class-themehunk-customize-control-tabs.php' );
 		include_once(plugin_dir_path(__FILE__) . 'themehunk/customizer-radio-image/class/class-themehunk-customize-control-radio-image.php' );
 		include_once(plugin_dir_path(__FILE__) . 'themehunk/customizer-scroll/class/class-themehunk-customize-control-scroll.php' );
 		themehunk_customizer_load_file();
 
-
 	}elseif(in_array("elanzalite", $theme)){
 		themehunk_customizer_load_file();
 		include_once( plugin_dir_path(__FILE__) . 'elanzalite/include.php' );
 	}
 	elseif(in_array("big-store", $theme) && class_exists('WooCommerce')){
+		require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
 		include_once( plugin_dir_path(__FILE__) . 'big-store/include.php' );
 	}
 		elseif(in_array("m-shop", $theme) && class_exists('WooCommerce') ){
+		require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
 		include_once( plugin_dir_path(__FILE__) . 'm-shop/include.php' );
 	}
-	elseif(in_array("jot-shop", $theme) && class_exists('WooCommerce')){
+	elseif(in_array("jot-shop", $theme) && class_exists('WooCommerce') && !function_exists('jot_shop_pro_text_domain')){
+		register_activation_hook( __FILE__, 'jot_shop_pro_deactivate' );
+		require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
 		include_once( plugin_dir_path(__FILE__) . 'jot-shop/include.php' );
 	}
-	elseif(in_array("amaz-store", $theme) && class_exists('WooCommerce')){
+	elseif(in_array("amaz-store", $theme) && class_exists('WooCommerce') && !function_exists('amaz_store_pro_text_domain')){
+		require_once( THEMEHUNK_CUSTOMIZER_PLUGIN_PATH . '/import/import.php' );
 		include_once( plugin_dir_path(__FILE__) . 'amaz-store/include.php' );
 	}
 }
+
 ?>

c96d47fce94d35d9f8b5fab37c12c723b32fba29
iheeresh <heereshd@gmail.com>
2025-09-16 17:52:00 +0530

    th wishlist added in big store .json

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '27248a4f468f7c66e5f8');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '488ef077d317af111cd7');

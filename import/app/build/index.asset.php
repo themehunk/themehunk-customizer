d683aefe8877de6d1967c622673846c6dd55e2d1
iheeresh <heereshd@gmail.com>
2026-06-13 14:15:46 +0530

    big store th store one added

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '488ef077d317af111cd7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '1e425f4ec8bf1f0c6d94');

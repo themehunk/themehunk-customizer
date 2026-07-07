bcf26d21e4843a33b8609564e7ed12745546cedd
iheeresh <heereshd@gmail.com>
2026-07-07 17:19:17 +0530

    modification

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'c6c7a6cb8abea72b7e8f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '8ec8a1eb39c641615a7b');
